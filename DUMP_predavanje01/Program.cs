6d0cdc931b18ea528b091b5d76bcc9e4bf2d66a3
duje <duje@CW5LAPTOP>
2017-03-19 18:13:15 +0100

    FTW

--- DUMP_predavanje01/Program.cs
+++ DUMP_predavanje01/Program.cs
@@ -10,7 +10,43 @@ namespace DUMP_predavanje01
     {
         static void Main(string[] args)
         {
+            int D1, D2, X1, X2;
 
+            do
+            {
+                Console.WriteLine("Od kada do kada Ivane ides na put:");
+                D1 = int.Parse(Console.ReadLine());
+                D2 = int.Parse(Console.ReadLine());
+            }while (!(D1 > 1 && D1 < 31 && D2 > 1 && D2 < 31));
+
+            do
+            {
+                Console.WriteLine("Koje ti je termine prijatelj ponudio za tulum Ivane (max 2 termina)?");
+                X1 = int.Parse(Console.ReadLine());
+                X2 = int.Parse(Console.ReadLine());
+            } while (!(X1 > 1 && X1 < 31 && X2 > 1 && X2 < 31));
+
+            Console.WriteLine();
+
+            if (X1 >= D1 && X1 <= D2)
+            {
+                Console.WriteLine("Ivane, ne mozes ici na zabavu " + X1 + ". dana u mjesecu");
+            }
+            else
+            {
+                Console.WriteLine("Ivane, mozes ici na zabavu " + X1 + ". dana u mjesecu");
+            }
+
+            if (X2 >= D1 && X2 <= D2)
+            {
+                Console.WriteLine("Ivane, ne mozes ici na zabavu " + X2 + ". dana u mjesecu");
+            }
+            else
+            {
+                Console.WriteLine("Ivane, mozes ici na zabavu " + X2 + ". dana u mjesecu");
+            }
+
+            Console.WriteLine();
         }
     }
 }
